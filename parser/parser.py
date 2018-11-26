0f575ffd458d60aee9b5ba6f13d53549c1fc15c3
Sean Pertet <sean.pertet@student.manchester.ac.uk>
2018-11-26 15:30:19 +0000

    Removed ANTLR files

--- parser/parser.py
+++ parser/parser.py
@@ -1,9 +1,29 @@
 import yaml
+import subprocess
+import cwltool
 
-stream = open('compile.cwl', 'r')
+stream = open('hello.cwl', 'r')
+p = subprocess.run(["cwltool", "--pack", "hello.cwl"], capture_output=True)
 
-workflow = yaml.safe_load(stream)
+if(p.returncode != 0) :
+  print ("Something went wrong")
+else :
+  workflow = yaml.safe_load(p.stdout.decode("utf8"))
+  print (workflow)
 
-for input in workflow["$graph"] :
-	print (input)
-	print ("\n")
+# wf = {}
+
+
+
+"""for step in workflow["steps"] :
+	s = {}
+	if("id" in step) :
+	  print (step["id"])
+	 s["name"] = step["id"]  or "step"
+	print ("Inputs:")
+	for input_ in step["in"] :
+	  if("id" in input_) :
+	    print (input_["id"])
+	  if("name" in input_) :
+	  	print (input_["name"])
+	print ("\n")"""
