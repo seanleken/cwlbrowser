3b9ab40cd6f763a07fcb45fd41ace65035fe5031
Sean Pertet <sean.pertet@student.manchester.ac.uk>
2018-11-26 15:34:31 +0000

    Removed keep folder

--- parser/keep/customcwlListener.py
+++
@@ -1,8 +0,0 @@
-import sys
-from antlr4 import *
-from cwlParser import cwlParser
-from cwlListener import cwlListener
-
-class customcwlListener(cwlListener) :
-    def __init__(self, output):
-        self.output = output
