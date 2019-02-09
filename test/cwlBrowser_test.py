7e05359d9bed40ab051d3fa5e0511b43e1cc63b1
Sean Pertet <sean.pertet@student.manchester.ac.uk>
2019-02-09 23:06:57 +0000

    Created subdirectories for each file type

--- test/cwlBrowser_test.py
+++ test/cwlBrowser_test.py
@@ -1,9 +1,24 @@
 import unittest
 import cwlBrowser as c
+import util
+import workflow
 
 class WorkFlowBrowserTest(unittest.TestCase) :
-	def test_loading(self):
-		link = "https://github.com/seanleken/workflowtest/blob/master/workflows/arithmeticfunction1.cwl"
-		
-		c.load(link, link=True)
-		self.assertTr
+	def test_input_instantiation(self):
+		inputs = {}
+		resultArray = []
+		workFlowGraph = {}
+		inputs['input1'] = {'type' : 'string', 'doc' : 'First input'}
+		inputs['input2'] = {'type' : 'string', 'doc' : 'Second input'}
+		inputs['input3'] = {'type' : 'string', 'doc' : 'Third input'}
+		resultArray = util.instantiateInputs(inputs, workFlowGraph, step=False)
+		i = 0
+		for result in resultArray :
+			i = i + 1
+			self.assertTrue(result.name == "input{}".format(i))
+			self.assertTrue(result.type == "string")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
